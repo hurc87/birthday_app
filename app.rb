d13d9461cfa405279d98479f22997000d24e64e4
hurc87 <zoe1987@btinternet.com>
2018-10-18 14:31:08 +0100

    birthday app working

---
+++ app.rb
@@ -0,0 +1,42 @@
+require 'sinatra'
+require 'Date'
+
+get '/' do
+  erb(:index)
+end
+
+get '/countdown' do
+  day = params[:day]
+  month = params[:month]
+  year = params[:year]
+  dayi = day.to_i
+  monthi = month.to_i
+  yeari = year.to_i
+  p day
+  p month
+  p year
+  today = Date.today
+  birthday = Date.new(yeari, monthi, dayi)
+  time_until = birthday - today
+  p time_until.to_i
+  @days = time_until.to_i.to_s
+
+
+  # @date == Date.today
+  if time_until.to_i == 0
+    erb :birthday
+  elsif time_until.to_i == 1
+    erb :countdown_1
+  else
+    erb :countdown
+  end
+end
+
+# get '/countdown_1' do
+#   erb :countdown_1
+# end
+
+get '/birthday' do
+  "This will be the landing page if your birthday is today"
+  erb :birthday
+end
